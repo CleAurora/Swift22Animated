1ec5e574b8a17eae033d1c7e0b44e2668e3ebb9c
Cleís Aurora Silveira Pereira <cleispereira@intelitrader.com.br>
2020-11-03 09:13:17 -0300

    Finish exercise

---
+++ Animation/Animation/UIColor+Random.swift
@@ -0,0 +1,19 @@
+//
+//  UIColor+Random.swift
+//  Animation
+//
+//  Created by Cleís Aurora Pereira on 03/11/20.
+//
+
+import UIKit
+
+extension UIColor {
+    static var random: UIColor {
+        return UIColor(
+            red: .random(in: 0...1),
+            green: .random(in: 0...1),
+            blue: .random(in: 0...1),
+            alpha: 1.0
+        )
+    }
+}
--- Animation/Animation/ViewController.swift
+++ Animation/Animation/ViewController.swift
@@ -11,43 +11,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    var newY = 40
+    var newY: CGFloat = 40
+    var newX: CGFloat = 0
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        createFirstView()
+    }
+
+    private func createFirstView() {
+        newY = 40
+        newX = 0
         let firstView = UIView(frame: CGRect(x:0, y: newY, width: 50, height: 50))
 
-
-        firstView.backgroundColor = .red
+        firstView.backgroundColor = .random
         firstView.isUserInteractionEnabled =  true
 
         let tap = UITapGestureRecognizer(target: self, action: #selector(addNewView))
         firstView.addGestureRecognizer(tap)
 
         view.addSubview(firstView)
-
-        let frame = UIScreen.main.bounds
-
-        UIView.animate(withDuration: 3){
-        }
-
-
-
-
     }
 
-    @objc func addNewView(){
+    @objc func addNewView() {
         newY += 55
 
+        if (newY + 50) >= view.frame.height {
+            newY = 40
+            newX += 55
+        }
 
-        let newView = UIView(frame: CGRect(x:0, y: newY, width: 50, height: 50))
+        if (newX + 50) >= view.frame.width {
+            let finalView = UIView(frame: CGRect(x: view.frame.midX, y: view.frame.midY, width: 50, height: 50))
+            finalView.backgroundColor = .random
+            view.addSubview(finalView)
+
+            UIView.animate(withDuration: 1) {
+                finalView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
+            } completion: { isCompleted in
+                if isCompleted {
+                    UIView.animate(withDuration: 1) {
+                        self.view.subviews.forEach { subview in
+                            subview.removeFromSuperview()
+                        }
+                        self.createFirstView()
+                    }
+                }
+            }
+
+            return
+        }
+
+        print(newX, newY, view.frame)
+
+        let newView = UIView(frame: CGRect(x: newX, y: newY, width: 50, height: 50))
 
 //        if newView >= frame {
 //            <#code#>
 //        }
 
-        newView.backgroundColor = .blue
+        newView.backgroundColor = .random
 
 
         let tap = UITapGestureRecognizer(target: self, action: #selector(addNewView))
